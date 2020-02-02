9c6c1a354e9f17c8b615ba06bf2fa431c8eb105b
Gian Marco Toso <gianmarco.toso@gmail.com>
2020-02-02 12:44:01 +0100

    update build files, update babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack')
 var path = require('path')
 
 module.exports = {
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 	context: path.join(__dirname, 'src'),
 	entry: [path.join(__dirname, 'src', 'index.js')],
 	output: {
@@ -9,32 +10,32 @@ module.exports = {
 		filename: 'index.js',
 
 		library: 'react-ui-formalize',
-		libraryTarget: 'umd'
+		libraryTarget: 'umd',
 	},
 	module: {
 		rules: [
 			{ test: /\.jsx?$/, exclude: /node_modules/, use: ['babel-loader'] },
-		]
+		],
 	},
 	resolve: {
 		extensions: ['.js', '.jsx', '.json'],
-		modules: ['node_modules', 'src']
+		modules: ['node_modules', 'src'],
 	},
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env': {
-				NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-			}
-		})
+				NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+			},
+		}),
 	],
 	externals: {
-		"react": "react"
+		react: 'react',
 	},
 	devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : '',
 	node: {
 		//console: 'empty',
 		fs: 'empty',
 		net: 'empty',
-		tls: 'empty'
-	}
+		tls: 'empty',
+	},
 }
